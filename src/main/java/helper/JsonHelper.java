9d8fe755819e7ebbf9839e36487aaab45eb18831
priyank <priyank.shah@thecarousell.com>
2022-02-26 23:45:38 +0530

    Added unit test cases

--- src/main/java/helper/JsonHelper.java
+++ src/main/java/helper/JsonHelper.java
@@ -8,18 +8,17 @@ import java.net.URL;
 import java.util.Optional;
 
 public class JsonHelper {
-    public static <T> T readJsonFile(String fileName, Class<T> clazz) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        File file = getFile(fileName);
-        return mapper.readValue(file, clazz);
-    }
+  public static <T> T readJsonFile(String fileName, Class<T> clazz) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    File file = getFile(fileName);
+    return mapper.readValue(file, clazz);
+  }
 
-    private static File getFile(String fileName) throws IOException {
-        ClassLoader classLoader = JsonHelper.class.getClassLoader();
-        URL found = Optional.ofNullable(classLoader.getResource(fileName))
-                .orElseThrow(() -> new IOException("File not found"));
-        return new File(found.getFile());
-    }
+  private static File getFile(String fileName) throws IOException {
+    ClassLoader classLoader = JsonHelper.class.getClassLoader();
+    URL found =
+        Optional.ofNullable(classLoader.getResource(fileName))
+            .orElseThrow(() -> new IOException("File not found"));
+    return new File(found.getFile());
+  }
 }
-
-
--- src/test/java/helper/JsonHelperTest.java
+++ src/test/java/helper/JsonHelperTest.java
@@ -5,13 +5,21 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIOException;
 
 class JsonHelperTest {
 
-    @Test
-    void readJsonFile() throws IOException {
-        TestRecord[] testRecord = JsonHelper.readJsonFile("test.json", TestRecord[].class);
-        assertThat(testRecord).isNotNull();
-        assertThat(testRecord[0].getColor()).isEqualTo("red");
-    }
+  @Test
+  void readValidJsonFile() throws IOException {
+    TestRecord[] testRecord = JsonHelper.readJsonFile("test.json", TestRecord[].class);
+    assertThat(testRecord).isNotNull();
+    assertThat(testRecord[0].getColor()).isEqualTo("red");
+  }
+
+  @Test
+  void shouldThrowExceptionIfJsonNotExists() {
+    assertThatIOException()
+        .isThrownBy(() -> JsonHelper.readJsonFile("invalid.json", TestRecord[].class))
+        .withMessageContaining("File not found");
+  }
 }
